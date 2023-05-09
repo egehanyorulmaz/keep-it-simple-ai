16ddf1688aadbd36ec5d2025121e441eca1a938a
egehanyorulmaz <egehanyorulmaz@uchicago.edu>
2023-05-09 12:48:58 -0500

    code refactoring

--- pdf_drive_crawler/helpers/Crawlers.py
+++ pdf_drive_crawler/helpers/Crawlers.py
@@ -7,39 +7,32 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException
 
 from bs4 import BeautifulSoup
 
+CHROME_DRIVER_PATH = "/usr/bin/chromedriver"
+
 class PDFDriveCrawler:
-    """Class to crawl PDFDrive and download books"""
+    """A class to crawl PDFDrive and download books."""
 
     def __init__(self, url: str):
+        """Initialize the PDFDriveCrawler with the base URL."""
         self.base_url = url
-        self.driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver' )
+        self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
         self.wait = WebDriverWait(self.driver, 10) # 10 seconds timeout
         self.promotion_appear_counter = 0
         self.start_crawling()
 
+    # Public methods
     def start_crawling(self) -> None:
-        """
-        Start crawling the website
-        """
+        """Open the base URL and maximize the window."""
         self.driver.get(self.base_url)
         self.driver.maximize_window()
         time.sleep(10)
 
-    def get_soup(self, _type='lxml') -> BeautifulSoup:
-        """Get the soup of the page"""
-        # Parse the HTML source with BeautifulSoup
-        soup = BeautifulSoup(self.driver.page_source, _type)
-        return soup
-
-    def get_html_source(self) -> str:
-        """Get the html source of the page"""
-        html_source = self.driver.page_source
-        return html_source
-
     def check_and_pass_promotion(self, wait_time = 5) -> None:
+        """Close the promotion pop-up if it appears."""
         wait = WebDriverWait(self.driver, 5)
         try:
             # Wait for the element to be visible
@@ -48,12 +41,12 @@ class PDFDriveCrawler:
             # Click on the element
             element.click()
             print("Promotion pop-up passed")
-            
-        except:
+        except TimeoutException:
             print("No promotion pop-up found")
         self.promotion_appear_counter += 1
 
     def search_book(self, book_name) -> None:
+        """Search for a book by its name."""
         if self.promotion_appear_counter >= 3:
             self.driver.refresh()
 
@@ -63,104 +56,39 @@ class PDFDriveCrawler:
         print(f'Entering book name {book_name}')
 
     def click_book_details(self, book_index=1) -> None:
-        """
-        Click on the book details
-        """
+        """Click on a book's details by its index."""
         self.check_and_pass_promotion()
-
-        # url = self.get_the_first_book_hyperlink()
-        url = self.get_book_hyperlink_by_position(book_index)
+        url = self.__get_book_hyperlink_by_position(book_index)
         self.driver.get(url)
-
-        # Wait for the page's title to be present
         self.wait.until(EC.presence_of_element_located((By.TAG_NAME, 'title')))
         print("Clicked on the book details")
-    
-    def __get_the_first_book_hyperlink(self) -> str:
-        """
-        Get the hyperlink of the first book
-        """
-        soup = self.get_soup()
-        
-        # Find the 'a' tag with the class 'ai-search'
-        a_tag = soup.find('a', class_='ai-search')
-
-        # Extract the hyperlink from the 'a' tag
-        hyperlink = a_tag['href']
-
-        print(hyperlink)
-        url = self.base_url + hyperlink
-        return url
-    
-    def get_book_hyperlink_by_position(self, book_index) -> str:
-        soup = self.get_soup()
-        # Find the 'a' tag using the provided CSS selector
-        CSS_SELECTOR = f"""body > div.dialog > div.dialog-main > div.dialog-left > div.files-new > ul > 
-        li:nth-child({book_index}) > div > div > div.file-right > a"""
-
-        a_tag = soup.select_one(CSS_SELECTOR)
-        # Extract the href attribute from the 'a' tag
-        hyperlink = a_tag['href']
-        print("Book hyperlink is as follows:", hyperlink)
-
-        url = self.base_url + hyperlink
-        return url
 
     def click_download_button(self) -> None:
-        # Get the entire HTML source of the current page
-        url =  self.get_download_button_hyperlink()
+        """Click the download button for the current book."""
+        url =  self.__get_download_button_hyperlink()
         self.driver.get(url)
-
-        # Wait for the page's title to be present
         self.wait.until(EC.presence_of_element_located((By.TAG_NAME, 'title')))
         print("Clicked to download button")
 
-    def get_download_link(self) -> str:
-        wait = WebDriverWait(self.driver, 20) # 10 seconds timeout
-
-        # Wait for the button to be visible and clickable
-        button = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@class="btn btn-primary btn-user"]')))
-
-        # Extract the hyperlink from the button
-        hyperlink = button.get_attribute('href')
-        print(hyperlink)
-
-        return hyperlink
-    
-    def get_download_button_hyperlink(self) -> str:
-        """
-        Click the download button in the book details page
-        """
-        soup = self.get_soup()
-        # Find the 'a' tag with the id 'download-button-link'
-        a_tag = soup.find('a', id='download-button-link')
-        # Extract the href attribute from the 'a' tag
-        hyperlink = a_tag['href']
-            
-        url = self.base_url + hyperlink
-        return url
-    
     def download_book(self) -> None:
+        """Download the current book."""
         self.check_and_pass_promotion()
         wait = WebDriverWait(self.driver, 20) # 10 seconds timeout
-
-        # Get the entire HTML source of the current page
-        url = self.get_download_link()
-
+        url = self.__get_download_link()
         print(url)
         self.driver.get(url)
-
-        # Wait for the page's title to be present
         wait.until(EC.presence_of_element_located((By.TAG_NAME, 'title')))
 
     def check_book_file_type(self) -> bool:
-        soup = self.get_soup(_type='html.parser')
+        # TODO: check if the book is in PDF format
+        """Check if the current book is in EPUB format."""
+        soup = self.__get_soup(_type='html.parser')
         text = soup.get_text().strip()
-
         is_epub = 'EPUB' in text
         return is_epub
-
+    
     def get_books_after_search(self) -> pd.DataFrame:
+        """Get a DataFrame of the books after searching."""
         data_collection = {"title": [], "year": []}
         for position in range(1, 21):
             try:
@@ -187,6 +115,8 @@ class PDFDriveCrawler:
         return df
 
     def get_most_similar_title(self, df, book_name, threshold=0.3) -> int:
+        """Return the index of the most similar book title in the DataFrame."""
+
         from nltk.corpus import stopwords
         from nltk.tokenize import word_tokenize
         import string
@@ -207,7 +137,6 @@ class PDFDriveCrawler:
 
         # Return the index of the first row with a distance above the threshold
         try:
-            print(result_index)
             result_index = df.index[df['distance'] > threshold].tolist()[0]
             if result_index >= 3:
                 # if the filtered book is after the 3rd place, then it is not available
@@ -215,10 +144,11 @@ class PDFDriveCrawler:
             return result_index
         except:
             return -1
-    
+
     def check_if_book_is_available(self, book_name):
+        """Check if a book is available on the website."""
         df = self.get_books_after_search()
-        idx =self.get_most_similar_title(book_name)
+        idx = self.get_most_similar_title(book_name)
 
         if idx == -1:
             # book is not available on the website
@@ -227,3 +157,68 @@ class PDFDriveCrawler:
             # book is available on the website
             return idx
 
+    # Private methods
+    def __get_soup(self, _type='lxml') -> BeautifulSoup:
+        """Return the BeautifulSoup object for the current page source."""
+        # Parse the HTML source with BeautifulSoup
+        soup = BeautifulSoup(self.driver.page_source, _type)
+        return soup
+
+    def __get_html_source(self) -> str:
+        """Return the HTML source of the current page."""
+        html_source = self.driver.page_source
+        return html_source
+
+    def __get_the_first_book_hyperlink(self) -> str:
+        """
+        OUTDATED
+        Return the hyperlink of the first book.
+        """
+        soup = self.__get_soup()
+            # Find the 'a' tag with the class 'ai-search'
+        a_tag = soup.find('a', class_='ai-search')
+
+        # Extract the hyperlink from the 'a' tag
+        hyperlink = a_tag['href']
+
+        print(hyperlink)
+        url = self.base_url + hyperlink
+        return url
+
+    def __get_book_hyperlink_by_position(self, book_index) -> str:
+        """Return the hyperlink of the book at a given index."""
+        soup = self.__get_soup()
+        # Find the 'a' tag using the provided CSS selector
+        CSS_SELECTOR = f"""body > div.dialog > div.dialog-main > div.dialog-left > div.files-new > ul > 
+        li:nth-child({book_index}) > div > div > div.file-right > a"""
+
+        a_tag = soup.select_one(CSS_SELECTOR)
+        # Extract the href attribute from the 'a' tag
+        hyperlink = a_tag['href']
+        print("Book hyperlink is as follows:", hyperlink)
+
+        url = self.base_url + hyperlink
+        return url
+
+    def __get_download_link(self) -> str:
+        wait = WebDriverWait(self.driver, 20) # 10 seconds timeout
+
+        # Wait for the button to be visible and clickable
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[@class="btn btn-primary btn-user"]')))
+
+        # Extract the hyperlink from the button
+        hyperlink = button.get_attribute('href')
+        print(hyperlink)
+
+        return hyperlink
+
+    def __get_download_button_hyperlink(self) -> str:
+        """Return the hyperlink of the download button."""
+        soup = self.__get_soup()
+        # Find the 'a' tag with the id 'download-button-link'
+        a_tag = soup.find('a', id='download-button-link')
+        # Extract the href attribute from the 'a' tag
+        hyperlink = a_tag['href']
+            
+        url = self.base_url + hyperlink
+        return url
