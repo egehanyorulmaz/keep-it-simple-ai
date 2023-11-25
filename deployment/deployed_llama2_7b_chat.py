1bd22b376164fd1d933d168c0ac7f542c0dac7ac
egehanyorulmaz <egehanyorulmaz@uchicago.edu>
2023-11-25 04:17:10 +0000

    adds ai-safety endpoint with hate-bert

--- deployment/deployed_llama2_7b_chat.py
+++ deployment/deployed_llama2_7b_chat.py
@@ -3,20 +3,29 @@ from datasets import load_dataset
 from functools import partial
 import os
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoTokenizer, AutoModelForSequenceClassification, TextClassificationPipeline
 import pandas as pd
+from collections import defaultdict
 from flask import Flask, request, Response, jsonify
 from flask_cors import CORS
 from helpers.chat_model_prompt_generator import LanguageLevelAssistant
 
+### SIMPLIFICATION MODEL ###
 PATH = "gs://kisai-data-msca310019-capstone/Text_Simplification/finetuned_llama2_responses.csv"
-model_name = "meta-llama/Llama-2-7b-chat-hf" 
+model_name = "meta-llama/Llama-2-7b-chat-hf"
 seed = 42
+assistant = LanguageLevelAssistant(model_name)
+
+
+### HATE SPEECH DETECTION ###
+tokenizer = AutoTokenizer.from_pretrained("tomh/toxigen_hatebert")
+model = AutoModelForSequenceClassification.from_pretrained("tomh/toxigen_hatebert")
+hatespeech_pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer)
+safety_probability_threshold = 0.2
 
 
 app = Flask(__name__)
 CORS(app)
-assistant = LanguageLevelAssistant(model_name)
 
 
 # Health check route
@@ -41,5 +50,34 @@ def predict():
         "simplified_text": simplified_text
     })
 
+# Predict route
+@app.route("/aisafety", methods=["POST"])
+def ai_safety():
+    print("/aisafety")
+    req_json = request.get_json()
+    context = req_json["text"]
+    hs_predictions = hatespeech_pipe.predict(context)[0]
+    
+    probabilities = defaultdict()
+    for output in hs_predictions:
+        if output['label']=='LABEL_0':
+            # HATE SPEECH PROBABILITY
+            probabilities["hs_prob"] = output["score"]
+        elif output['label']=='LABEL_1':
+            # NOT HATE SPEECH PROBABILITY
+            probabilities["non_hs_prob"] = output["score"]
+    
+    
+    if probabilities["non_hs_prob"] - probabilities["hs_prob"] > safety_probability_threshold:
+        output = "safe"
+    elif probabilities["non_hs_prob"] - probabilities["hs_prob"] > 0:
+        output = "not_sure_but_safe"
+    else:
+        output = "not_safe"
+    
+    return jsonify({
+        "result": output})
+        
+        
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8081, use_reloader=False)
